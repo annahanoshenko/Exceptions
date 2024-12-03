a4f12a4ac18201509c06e6a5cc672af0b5b85b0f
Anna Hanoshenko <hanoshenkoanna@gmail.com>
2024-12-03 16:40:31 +0100

    Exceptions

---
+++ ExceptionsPractice/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsPractice
+{
+    public class CustomException : Exception
+    {
+        public CustomException(string massege) : base(massege) { }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new CustomException("This is a custom exception.");
+            }
+                catch(CustomException ex)
+                {
+                    Console.WriteLine($"Custom error: {ex.Message}");
+                }
+
+            try
+            {
+                throw new Exception("Smth went wrong.");
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.WriteLine($"StackTrace: {ex.StackTrace}");
+            }
+
+            try
+            {
+                int[] numbers = { 1, 2, 3 };
+                Console.WriteLine(numbers[5]);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            try
+            {
+                string contetnt = File.ReadAllText("nonexistent.txt");
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            try
+            {
+                int number = int.Parse("abc");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            try
+            {
+                throw new Exception("Er");
+            }
+            catch (Exception ex) when (ex.Message.Contains("Error"))
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            try
+            {
+                throw new InvalidOperationException("Custom message");
+            }
+            catch (InvalidOperationException ex) when (ex.Message.Contains("Custom"))
+            {
+                Console.WriteLine("Caught specific InvalidOperationException with 'Custom' in the message.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Caught a general exception: {ex.Message}");
+            }
+
+        }
+    }
+}
--- TransportManager/Program.cs
+++ TransportManager/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,20 @@ namespace TransportManager
     {
         static void Main(string[] args)
         {
+            try
+            {
+                throw new InvalidOperationException("Custom message");
+            }
+            catch (InvalidOperationException ex) when (ex.Message.Contains("Custom"))
+            {
+                Console.WriteLine("Caught specific InvalidOperationException with 'Custom' in the message.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Caught a general exception: {ex.Message}");
+            }
+                Console.ReadKey();
+
         }
     }
 }
